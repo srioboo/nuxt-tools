dc3e63570f64a26fa27cc240d432a7ccfcf664f6
salrio <salrio74@gmail.com>
2020-04-18 13:23:32 +0200

    actualizacion de header y cambio de clase a modo normal

---
+++ src/components/Header.vue
@@ -0,0 +1,93 @@
+<template>
+    <header>
+    <div class="burger">
+        <div class="line1"></div>
+        <div class="line1"></div>
+        <div class="line1"></div>
+    </div>
+
+    <div class="logo">
+        The Logo
+    </div>
+
+    <div>
+        <form class="buscador form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+    </div>
+
+    </header>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'Header',
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      text: 'Text',
+    };
+  },
+  /* methods: {
+    sayhello() {
+      console.log('Hello!!');
+    },
+  }, */
+});
+
+</script>
+
+<style lang="scss" scoped>
+@import '../css/_variables.scss';
+
+    header {
+    background-color: $background-main;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    color: $white;
+    height: 50px;
+    align-items: center;
+
+    .logo {
+        font-weight: bolder;
+        font-family: sans-serif;
+    }
+
+    .buscador {// TODO: adaptar
+
+        display: flex;
+        justify-content: space-around;
+        align-self: center;
+        width: 50%;
+
+        li {
+            text-decoration: none;
+            list-style: none;
+        }
+    }
+
+    .burger {
+        width: 40px;
+        height: 24px;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-around;
+        align-self: center;
+        align-items: center;
+        cursor: pointer;
+
+        div {
+            display: block;
+            height: 3px;
+            width: 25px;
+            background-color: $white;
+        }
+    }
+}
+</style>
---
+++ src/components/Menu.vue
@@ -0,0 +1,61 @@
+<template>
+<nav>
+  <ul class="menu">
+    <li class="nav-item">
+      <div class="nav-link">Ver todos Menu</div>
+    </li>
+    <li class="nav-item">
+      <div class="nav-link">
+          Hola mundo
+      </div>
+    </li>
+  </ul>
+</nav>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'Menu',
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+
+    };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+    @import '../css/_variables.scss';
+
+    nav {
+    background-color: $background-main;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    color: white;
+    //height: 50px;
+    align-items: center;
+
+
+    .menu {
+        margin: 10px 10px 10px 20px;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+        align-self: center;
+        min-width: 25vw;
+
+        li {
+            text-decoration: none;
+            list-style: none;
+            margin: 5px 0px;
+            height: 50px;
+        }
+    }
+}
+</style>
