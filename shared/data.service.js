37540363bcba37cf0a5c4384e3bc82f961a66f5c
Salvador Rioboo Naranjo <salvador.rioboo@mayoral.com>
2023-07-22 19:46:41 +0200

    fix: build

--- shared/data.service.js
+++ shared/data.service.js
@@ -1,9 +1,10 @@
-import axios from 'axios';
 import { API } from './config';
 
 const getBookmarks = async function() {
   try {
-    const response = await axios.get(`${API}/bookmarks`);
+    // const response = await axios.get(`${API}/bookmarks`);
+
+    const response = await $fetch.get(`${API}/bookmarks`);
 
     // this.$content
 
