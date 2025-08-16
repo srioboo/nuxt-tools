4364d9bc1fca500851fea0c5e0875029b74d5676
Salvador Rioboo Naranjo <salrio74@gmail.com>
2025-08-16 23:22:42 +0200

    feat: add some styles

--- app/components/Menu.vue
+++ app/components/Menu.vue
@@ -1,6 +1,6 @@
 <template>
-  <nav id="navmenu">
-    <router-link tag="div" class="nav-link" to="/" exact>
+  <nav id="navmenu" class="w-1/5 h-screen font-extrabold">
+    <router-link tag="div" class="nav-link pl-2.5 h-12 flex items-center content-start border-b-1 border-b-green-950" to="/" exact>
       Bookmaks
     </router-link>
     <!--<router-link tag="div" class="nav-link" to="/bookmarks" exact>
@@ -23,89 +23,17 @@
   </nav>
 </template>
 
-<script>
-/*import Vue from 'vue';
-
-export default Vue.extend({
-  name: 'Menu',
-  props: {
-    msg: { type: String, required: false, default: '' },
-  },
-  data() {
-    return {};
-  },
-  methods: {
-    toggleShow(event) {
-      const elemento = event.target;
-      elemento.nextElementSibling.classList.toggle('hide');
-    },
-  },
-});*/
+<script setup lang="ts">
+function toggleShow(className: string) {
+  const nav = document.getElementById('navmenu');
+  if (nav) {
+    nav.classList.add(className);
+  }
+}
 </script>
 
-
-<style >
-/*
-nav {
- // box-shadow: 0px 0px 5px 0px $green;
- // color: $white;
-  width: 20vw;
-  height: 100vh;
-  font-weight: bolder;
-
-  &.hide {
-    display: none;
-  }
-
-  .sub-nav-group {
-    &.hide {
-      display: none;
-    }
-  }
-
-  .nav-link {
-    padding-left: 15px;
-    height: 50px;
-    display: flex;
-    align-items: center;
-    justify-content: flex-start;
-   // color: $black;
-   // border-bottom: 1px solid $green;
-*/
-   /* &.router-link-exact-active {
-      background-color: $green-drkr;
-      color: $white;
-    }
-
-    &.router-link-active {
-      background-color: $green-drkr;
-      color: $white;
-    }
-
-    &:hover {
-      background-color: $green;
-      color: $white !important;
-    }
-
-    &.sublink {
-      height: 30px;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      border-bottom: 0px;
-    }
-  }
-
-  .sub-nav-link {
-    display: flex;
-    justify-content: stretch;
-    flex-direction: row;
-    // color: $black;
-    height: 50px;
-    align-items: center;
-    padding-left: 30px;
-    // background-color: $green-lgt-70;
-    // border-bottom: 1px solid $primary;
-  }
-}*/
+<style>
+#navmenu {
+  display: hidden;
+}
 </style>
